612605edd817d77abdccd5ee0c20ee1ad7e000b6
Fox Scarlett <foxontheone@gmail.com>
2016-02-02 21:08:38 -0800

    Convert CRUD requests to use current user todos only.

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/', function (req, res) {
 // GET /todos?completed=true&q=house
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	}; 
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -44,12 +46,17 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function (todo) {
-		if (!!todo) {
-			res.json(todo.toJSON());
-		} else {
-			res.status(404).json({"error": "No todo found with that Id."});
-		}
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+			}
+		}).then(function (todo) {
+			if (!!todo) {
+				res.json(todo.toJSON());
+			} else {
+				res.status(404).json({"error": "No todo found with that Id."});
+			}
 	}, function (e) {
 		res.status(500).json(e);
 	});
@@ -76,7 +83,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,   
+			userId: req.user.get('id')
 		}
 	}).then(function (rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -105,7 +113,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function (todo) {
+	db.todo.findOne({
+			where: {
+				id: todoId,
+				userId: req.user.get('id')
+			}
+		}).then(function (todo) {
 		if (todo) {
 			todo.update(attributes).then(function (todo) {
 				res.json(todo.toJSON());
@@ -148,9 +161,11 @@ app.post('/users/login', function (req, res) {
 	});
 });
 
-db.sequelize.sync({force: true}).then(function () {
-	app.listen(PORT, function () {
-		console.log('Express listening on port ' + PORT + '.');
+db.sequelize.sync({
+		// force: true
+	}).then(function () {
+		app.listen(PORT, function () {
+			console.log('Express listening on port ' + PORT + '.');
 	});
 });
 
